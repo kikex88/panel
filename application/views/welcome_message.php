c3f8d3134fdc2cfc908a7ebec066fbdcb914d644
kikex88 <enrique.qzada@gmail.com>
2019-01-22 14:25:07 -0600

    cambios

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -3,10 +3,10 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Panel</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css') ?>">
+	<script src="<?php echo base_url('assets/js/jquery-3.3.1.min.js') ?>"></script>
+	<script src="<?php echo base_url('assets/js/popper.min.js') ?>"></script>
+	<script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
 
 
 <style>
@@ -96,7 +96,7 @@
   </button>
   <a class="navbar-brand" href="#">
     <div class="circular">
-        <img class="circular" src="https://v4-alpha.getbootstrap.com/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+        <img class="circular" src="2.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
     </div>
     <span class="menu-collapsed">Usuario</span>
   </a>
